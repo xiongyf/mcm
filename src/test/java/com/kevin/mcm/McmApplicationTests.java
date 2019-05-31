dd25445cb12ea49bffe663975c49540292e78312
xiongyf <xiongyf1@gmail.com>
2019-05-31 23:08:00 +0800

    edit properties

--- src/test/java/com/kevin/mcm/McmApplicationTests.java
+++ src/test/java/com/kevin/mcm/McmApplicationTests.java
@@ -3,10 +3,13 @@ package com.kevin.mcm;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kevin.mcm.mm.entity.ConsumeRecord;
 import com.kevin.mcm.mm.service.IConsumeRecordService;
+import com.kevin.mcm.sys.BaseResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -17,11 +20,18 @@ public class McmApplicationTests {
     @Resource
     IConsumeRecordService consumeRecordService;
 
-    @Test
     public void contextLoads() {
         QueryWrapper<ConsumeRecord> wrapper = new QueryWrapper<>();
         wrapper.eq("member_id", "26");
         consumeRecordService.remove(wrapper);
+    }
+
+    @Test
+    public void restTest() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<BaseResult> res = restTemplate.getForEntity("http://localhost/mm/member/one?id=4", BaseResult.class);
+        System.out.println(res);
+        restTemplate.delete("http://localhost/mm/member/deleteById?id=4");
     }
 
 }
