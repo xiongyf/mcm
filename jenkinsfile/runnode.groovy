fa9344d06d291a129f09542d394a5876622b8033
xiongyf <xiongyf1@gmail.com>
2020-05-31 20:54:02 +0800

    update checkoout script

--- jenkinsfile/runnode.groovy
+++ jenkinsfile/runnode.groovy
@@ -1,6 +1,23 @@
 node{
     echo "begin"
+    def curPath=pwd();
+//    def jenkinsPipelinFolder="jenkinsPipeline"
+//    bat("/mkdir -p ${jenkinsPipelinFolder}")
+
+
     stage("run node file"){
-        bat(/node hellonode.js/)
+        dir("${curPath}"){
+
+            echo "checkout js script..."
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+                      doGenerateSubmoduleConfigurations: false,
+                      //extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenkinsPipeline']],
+                      submoduleCfg: [],
+                      userRemoteConfigs: [[credentialsId: '26c21ae3-d9f2-4ddd-b68d-ca4d38042eb7', url: 'https://github.com/xiongyf/mcm.git']]]
+            )
+            echo "run js script..."
+            bat(/node hellonode.js/)
+            echo "checkout js script end."
+        }
     }
 }
